7692cf4b6c0bf72c0d88c12d3ce173ffef49ead9
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2023-12-26 23:16:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,71 @@ st.title("DASHBOARD - GCINFRA AGIR")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(spreadsheet=url, usecols=list(range(6)))
+df = df.sort_values("DATA")
+
+# Convert the "DATA" column to datetime with errors='coerce'
+df["DATA"] = pd.to_datetime(df["DATA"], format='%d/%m/%Y', errors='coerce')
+
+
+# Filter out rows where the date could not be parsed (NaT)
+df = df.dropna(subset=["DATA DE ATRIBUIÇÃO:"])
+
+# Extract year, month, and quarter
+df["Year"] = df["DATA"].dt.year
+df["Month"] = df["DATA"].dt.month
+df["Quarter"] = df["DATA"].dt.quarter
+df["Semester"] = np.where(df["DATA"].dt.month.isin([1, 2, 3, 4, 5, 6]), 1, 2)
+
+# Create a "Year-Quarter" column
+df["Year-Quarter"] = df["Year"].astype(str) + "-T" + df["Quarter"].astype(str)
+
+# If you want to create a "Year-Month" column, you can use the following line
+df["Year-Month"] = df["DATA"].dt.strftime("%Y-%m")
+
+# Create a "Year-Quarter" column
+df["Year-Semester"] = df["Year"].astype(str) + "-S" + df["Semester"].astype(str)
+
+
+
+# Sort the unique values in ascending order
+unique_year_month = sorted(df["Year-Month"].unique())
+unique_year_quarter = sorted(df["Year-Quarter"].unique())
+unique_year_semester = sorted(df["Year-Semester"].unique())
+unique_year = sorted(df["Year"].unique())
+
+
+
+# Add "All" as an option for both filters
+unique_year_month.insert(0, "Todos")
+unique_year_quarter.insert(0, "Todos")
+unique_year_semester.insert(0, "Todos")
+unique_year.insert(0, "Todos")
+
+
+# Define the list of "ENTIDADE" values and add "Todos" as an option
+desired_classificacao = df["ENTIDADE"].unique().tolist()
+desired_classificacao.insert(0, "Todos")
+
+# Create a filter for selecting "ENTIDADE"
+classificacao = st.sidebar.selectbox("Entidade", desired_classificacao)
+
+# Define the list of "CLASSIFICAÇÃO" values and add "Todos" as an option
+desired_numero_processo = df["CLASSIFICAÇÃO"].unique().tolist()
+desired_numero_processo.insert(0, "Todos")
+
+# Create a filter for selecting "CLASSIFICAÇÃO"
+numero_processo = st.sidebar.selectbox("Classificação", desired_numero_processo)
+
+
+# Define the list of "CATEGORIA" values and add "Todos" as an option
+categoria = df["CATEGORIA"].unique().tolist()
+categoria.insert(0, "Todos")
+
+# Create a filter for selecting "CATEGORIA"
+numero_categoria = st.sidebar.selectbox("Categoria", categoria)
+
+
+
 
 st.write("Dados Selecionados:")
 st.dataframe(df)
