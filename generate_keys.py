5ccf287eabdb21ada275f582d0c8aad5ee193779
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-01-23 10:28:47 -0300

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -5,7 +5,7 @@ import streamlit_authenticator as stauth
 
 names = ["Peter Parker", "Rebecca Miller", "Pedro Borges", "Arthur Pires", "Kaio Razotto", "Vitor Peixoto", "Claudemiro Dourado", "Izabela Lopes", "Fernando Souza", "Michelle Bonfim"]
 usernames = ["pparker", "rmiller", "pborges", "arthur.pires", "kaio.razotto", "vitor.peixoto", "claudemiro.dourado", "izabela.lopes", "fernando.souza", "michelle.bonfim"]
-passwords = ["XXX", "XXX", "agirabc", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024" ]
+passwords = ["XXX", "XXX", "agirabc", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
