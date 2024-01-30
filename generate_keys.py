da84d1b48471f5b3d0b8f3cb8fc4cf29ae1de3ca
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-01-30 08:56:48 -0300

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,10 +3,9 @@ from pathlib import Path
 
 import streamlit_authenticator as stauth
 
-names = ["Peter Parker", "Rebecca Miller", "Pedro Borges", "Arthur Pires", "Kaio Razotto", "Vitor Peixoto", "Claudemiro Dourado", "Izabela Lopes", "Fernando Souza", "Michelle Bonfim", "Gerson Rodrigues"]
-usernames = ["pparker", "rmiller", "pborges", "arthur.pires", "kaio.razotto", "vitor.peixoto", "claudemiro.dourado", "izabela.lopes", "fernando.souza", "michelle.bonfim", "gerson.rodrigues"]
-passwords = ["XXX", "XXX", "agirabc", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024"]
-
+names = ["Peter Parker", "Rebecca Miller", "Pedro Borges", "Arthur Pires", "Kaio Razotto", "Vitor Peixoto", "Claudemiro Dourado", "Izabela Lopes", "Fernando Souza", "Michelle Bonfim", "Gerson Rodrigues", "Mônica Guimarães", "Pedro Andrade", "Jéssyca de Paula"]
+usernames = ["pparker", "rmiller", "pborges", "arthur.pires", "kaio.razotto", "vitor.peixoto", "claudemiro.dourado", "izabela.lopes", "fernando.souza", "michelle.bonfim", "gerson.rodrigues", "monica.guimaraes", "pedro.andrade", "jessyca.depaula"]
+passwords = ["XXX", "XXX", "agirabc", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024"]
 hashed_passwords = stauth.Hasher(passwords).generate()
 
 file_path = Path(__file__).parent / "hashed_pw.pkl"
